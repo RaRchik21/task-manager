0e4561d15c2e9a9b88b913bba265efcc178da22e
RaRchik21 <razars2408@gmail.com>
2026-05-10 12:08:33 +0300

    заработай пж

--- task_manager/wsgi.py
+++ task_manager/wsgi.py
@@ -11,6 +11,9 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
+import pymysql
+pymysql.install_as_MySQLdb()
+
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
 
 application = get_wsgi_application()
